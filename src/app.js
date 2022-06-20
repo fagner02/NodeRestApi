2f4b64d5ee632a835bdd7317057d7959f4c05b3b
Fagner <fagnerrc12@gmail.com>
2022-06-20 01:14:52 -0300

    Add post test

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.send("Hello World!" + res.statusCode);
 });
-
+router.post("/p", (req, res) => {
+  res.send("post");
+});
 app.use("/.netlify/functions/app", router);
 
 module.exports.handler = serverless(app);
